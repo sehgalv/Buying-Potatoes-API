a98e8360e2000db9e1f1cbc7a3eeee903ed3bb49
parrycj <parrycj@vcu.edu>
2017-12-09 11:34:18 -0500

    categories post delete

--- src/db/categories.js
+++ src/db/categories.js
@@ -64,6 +64,71 @@ module.exports.getItemCategories = function getItemCategories(connection, ITEM_I
     );
 };
 
+exports.postItemCategory = function postItemCategory(connection, itemCategoryPair){
+    return connection.execute(`
+        INSERT INTO BP_ITEM_CATEGORY
+        VALUES (
+            :ITEM_ID,
+            :CATEGORY_NAME
+        )`,
+            [itemCategoryPair.ITEM_ID,
+            itemCategoryPair.CATEGORY_NAME
+            ], {
+                autoCommit : true
+            })
+    .then(
+        (res) => {
+            if(res.rowsAffected === 0)  
+                return Promise.reject({
+                    location: `POST category item`,
+                    err: `Unsuccessful in adding category to item`
+                });
+            else
+                {
+                    return Promise.resolve({
+                        status: 200,
+                        data: `Successfully added category to item`
+                    });
+                }
+                
+        },
+        (err) => Promise.reject({
+            location: `POST category item`,
+            err: err
+        })
+    );
+};
+
+module.exports.deleteItemCategory = function deleteItemCategory(connection, itemCategoryPair) {
+    return connection.execute(
+        `DELETE FROM BP_ITEM_CATEGORY
+        WHERE ITEM_ID = :ITEM_ID
+        AND CATEGORY_NAME = :CATEGORY_NAME`, [itemCategoryPair.ITEM_ID, itemCategoryPair.CATEGORY_NAME], {
+            autoCommit: true
+        }
+    )
+    .then(
+        (res) => {
+            if(res.rowsAffected === 0)  
+                return Promise.reject({
+                    location: `DELETE category`,
+                    err: `Unsuccessful in deleting item category pair`
+                });
+            else
+                {
+                    return Promise.resolve({
+                        status: 200,
+                        data: `Successfully removed category`
+                    });
+                }
+                
+        },
+        (err) => Promise.reject({
+            location: `DELETE category`,
+            err: err
+        })
+    );
+};
 //DELETE ITEM-CATEGORY PAIR FROM BP_ITEM_CATEGORY
 /*
 DELETE FROM BP_ITEM_CATEGORY
