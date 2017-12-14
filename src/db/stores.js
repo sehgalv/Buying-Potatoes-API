3b42f674b275354ba03a02046a576488501a1602
Vishakha Sehgal <sehgalv@vcu.edu>
2017-12-14 07:47:11 -0500

    store route updated

--- src/db/stores.js
+++ src/db/stores.js
@@ -7,21 +7,112 @@ var oracledb = require('oracledb');
  */
 module.exports.getStores = function getStores(connection) {
     return connection.execute(
-        `SELECT * FROM BP_STORE`, [], {
+        `SELECT so.STORE_ID, s.STORE_NAME, s.ADDRESS_ID,
+        ad.STREET_NUMBER, ad.STREET_NAME, ad.APT_NUMBER, ad.CITY, ad.STATE_ABBREVIATION, ad.ZIP, ad.LATITUDE, ad.LONGITUDE,
+        s.PHONE_NUMBER, so.USER_ID, sh.WEEKDAY, sh.OPEN_TIME, sh.CLOSE_TIME
+        FROM BP_STORE_OWNER so left join BP_STORE s
+        on so.store_id = s.store_id
+        left join BP_ADDRESS ad
+        on s.ADDRESS_ID = ad.ADDRESS_ID
+        left join BP_STORE_HOURS sh
+        on so.store_id = sh.store_id`, [],{
             outFormat: oracledb.OBJECT
         }
     )
     .then(
         (res) => {
+              console.log(res.rows);
+              var tempStoreIds = [];
+            for (var i in res.rows) {                             
+                var storeInfo = {
+                    STORE_ID: res.rows[i].STORE_ID,
+                    STORE_NAME: res.rows[i].STORE_NAME,
+                    ADDRESS: {
+                        ADDRESS_ID: res.rows[i].ADDRESS_ID,
+                        STREET_NUMBER: res.rows[i].STREET_NUMBER,
+                        STREET_NAME: res.rows[i].STREET_NAME,
+                        APT_NUMBER: res.rows[i].APT_NUMBER,
+                        CITY: res.rows[i].CITY,
+                        STATE: res.rows[i].STATE_ABBREVIATION,
+                        ZIP: res.rows[i].ZIP,
+                        LATITUDE: res.rows[i].LATITUDE,
+                        LONGITUDE: res.rows[i].LONGITUDE
+                    },
+                    PHONE_NUMBER: res.rows[i].PHONE_NUMBER,
+                    USER_ID: res.rows[i].USER_ID
+                }
+                tempStoreIds.push(storeInfo);
+            }
+
+            var storeIds = tempStoreIds.reduce(function(a, b){
+                if (a.indexOf(b.STORE_ID) == -1) {
+                    a.push(
+                        b.STORE_ID)
+                }
+                return a;
+            }, []);
+            var addressOfStore = [];
+            
+            for (var j in storeIds) {
+                var storeHours = [];
+                var getStoreInfo =[];
+                for (var k in res.rows) {
+                    if(storeIds[j] === res.rows[k].STORE_ID) {
+                        getStoreInfo = {
+                            STORE_NAME: res.rows[k].STORE_NAME,
+                            ADDRESS: {
+                                ADDRESS_ID: res.rows[k].ADDRESS_ID,
+                                STREET_NUMBER: res.rows[k].STREET_NUMBER,
+                                STREET_NAME: res.rows[k].STREET_NAME,
+                                APT_NUMBER: res.rows[k].APT_NUMBER,
+                                CITY: res.rows[k].CITY,
+                                STATE: res.rows[k].STATE_ABBREVIATION,
+                                ZIP: res.rows[k].ZIP,
+                                LATITUDE: res.rows[k].LATITUDE,
+                                LONGITUDE: res.rows[k].LONGITUDE
+                            },
+                            PHONE_NUMBER: res.rows[k].PHONE_NUMBER,
+                            USER_ID: res.rows[k].USER_ID
+                        }
+
+                        storeHours.push({
+                            DAYOFWEEK: res.rows[k].WEEKDAY,
+                            OPEN: res.rows[k].OPEN_TIME,
+                            CLOSE: res.rows[i].CLOSE_TIME
+                        });
+                    }
+                }
+            
+        addressOfStore.push({
+                    STORE_ID: storeIds[j],
+                    STORE_NAME: getStoreInfo.STORE_NAME,
+                    ADDRESS: {
+                        ADDRESS_ID: getStoreInfo.ADDRESS.ADDRESS_ID,
+                        STREET_NUMBER: getStoreInfo.ADDRESS.STREET_NUMBER,
+                        STREET_NAME: getStoreInfo.ADDRESS.STREET_NAME,
+                        APT_NUMBER: getStoreInfo.ADDRESS.APT_NUMBER,
+                        CITY: getStoreInfo.ADDRESS.CITY,
+                        STATE: getStoreInfo.ADDRESS.STATE_ABBREVIATION,
+                        ZIP: getStoreInfo.ADDRESS.ZIP,
+                        LATITUDE: getStoreInfo.ADDRESS.LATITUDE,
+                        LONGITUDE: getStoreInfo.ADDRESS.LONGITUDE
+                    },
+                    PHONE_NUMBER: getStoreInfo.PHONE_NUMBER,
+                    USER_ID: getStoreInfo.USER_ID,
+                    HOURS: storeHours
+                });
+            }
+
+
             return Promise.resolve({
                 status: 200,
-                data: res.rows
-            })
+                data: addressOfStore
+            });
         },
         (err) => {
             return Promise.reject({
                 location: `Get stores`,
-                err: any,
+                err: 'get error',
             })
         }
     );
